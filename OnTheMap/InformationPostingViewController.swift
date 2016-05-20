f311d38a636c8e46edf15bc8eaaeddecd62cf9df
Reagan Wood <reaganwood1@gmail.com>
2016-05-20 12:59:11 -0500

    Added detail to informationViewController, beginning location finding for map

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -10,7 +10,27 @@ import Foundation
 import UIKit
 import MapKit
 
-class InformationPostingViewController: UIViewController, MKMapViewDelegate {
+class InformationPostingViewController: UIViewController, MKMapViewDelegate, UITextViewDelegate{
 
+    @IBOutlet weak var findOnMapButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var locationTextView: UITextView!
     
+    override func viewDidLoad() {
+        
+        // set locationTextView delegate
+        locationTextView.delegate = self
+        cancelButton.bringSubviewToFront(cancelButton)
+    }
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        
+        locationTextView.text = ""
+        locationTextView.textAlignment = .Left
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        // reposition the text at the end of editing
+        locationTextView.textAlignment = .Center
+    }
 }
